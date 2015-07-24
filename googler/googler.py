49e8706ee3877975acc05232b985569491ed8ad8
Todd Qualls <tqualls@gmail.com>
2015-07-23 22:30:31 -0400

    First commit with googler.py

---
+++ googler/googler.py
@@ -0,0 +1,14 @@
+import time
+happy=['smile','love','fun','CANDY!!!']
+for i in happy:
+	print(i)
+	print(i)
+	print(i)
+for j in happy:
+		print (j)
+		print (j)
+		print (j)
+		print (i+j)
+		happy.append (i+j)
+		print (len (happy))
+		time.sleep (.1)
